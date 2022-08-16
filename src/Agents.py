ac40ead1eb26a77f2cfa9a3148eb767acf596f7e
Tarnage <70882644+Tarnage@users.noreply.github.com>
2022-08-16 12:05:53 +0800

    init

---
+++ src/Agents.py
@@ -0,0 +1,21 @@
+class Agent:
+    def __init__(self):
+        pass
+
+
+class Grey_Agent(Agent):
+    def __init__(self):
+        super.__init__()
+        pass
+
+
+class Red_Agent(Agent):
+    def __init__(self):
+        super.__init__()
+        pass
+
+
+class Green_Agent(Agent):
+    def __init__(self):
+        super.__init__()
+        pass
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import Agents
+import numpy
+
+class Simulator:
+    pass
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    pass
