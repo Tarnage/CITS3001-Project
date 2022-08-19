aec1974c1500535f0711787927cbc48fd526371e
Tarnage <70882644+Tarnage@users.noreply.github.com>
2022-08-19 13:47:33 +0800

    created classes with variables we know

--- src/Agents.py
+++ src/Agents.py
@@ -1,21 +1,36 @@
-class Agent:
+class Grey_Agent():
+    def __init__(self, grey_proportion):
+        self.team_alignment = self.set_team_alignment(grey_proportion)
+
+    def set_team_alignment(self, proportion):
+        return
+
+class Red_Agent():
     def __init__(self):
-        pass
+        self.energy = 100
 
 
-class Grey_Agent(Agent):
+class Blue_Agent():
     def __init__(self):
-        super.__init__()
-        pass
+        self.energy = 100
 
 
-class Red_Agent(Agent):
+class Green_Agent():
     def __init__(self):
-        super.__init__()
-        pass
+        self.will_vote = 0.0
+        self.not_vote = 0.0
+    
+    def get_will_vote(self):
+        return self.will_vote
 
+    def get_not_vote(self):
+        return self.not_vote
 
-class Green_Agent(Agent):
-    def __init__(self):
-        super.__init__()
-        pass
+    def set_will_vote(self, value: int):
+        self.will_vote = value
+
+    def set_not_vote(self, value: int):
+        self.not_vote = value
+
+    def calculate_vote_status(self, interval: list):
+        return
--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
 import Agents
 import numpy
 
-class Simulator:
-    pass
+class Info_Simulator:
+    def __init__(self, num_green: int, uncert_ints: list, connect_prob: list, grey_proportion: int) -> None:
+        self.green_agent_adjlist = self.create_green_agents(num_green, uncert_ints, connect_prob)
+        self.red_agent = Agents.Red_Agent()
+        self.blue_agent = Agents.Blue_Agent()
+        self.grey_agent = Agents.Grey_Agent(grey_proportion)
+
+    
+    def create_green_agents(num_green: int, uncert_ints: list, connect_prob: list) -> list:
+        # TODO:
+        # Init num_green agents
+        # Apply uncert_ints to will_vote and not_vote
+        # Use connect_prob to create the adjlist of green agents connections
+        
+        return
 
 
-def main(args):
-    pass
+    def run():
+
+        finished = False
+        while not finished:
+            break
+
 
 
 if __name__ == "__main__":
-    pass
+    sim = Info_Simulator()
+    sim.run()
