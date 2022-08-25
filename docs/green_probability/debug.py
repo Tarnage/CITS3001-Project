d717cf58eb4560036092d69f6d4968f1fb34c918
Tom <1991tnguyen@gmail.com>
2022-08-25 14:54:53 +0800

    adjust n p calc

--- docs/green_probability/debug.py
+++ docs/green_probability/debug.py
@@ -12,11 +12,15 @@ rand.seed(TEST_SEED)
 # Green Agent Class
 # We must approriatley apply random seeds
 
+# Green Agent Class
+# We must approriatley apply random seeds
+
 class Green_Agent():
     def __init__(self, connection_prob: list):
         self.will_vote = 0.0
         self.not_vote = 0.0
         self.n, self.p = connection_prob[0], connection_prob[1]
+        self.connections = list()
     
     def get_will_vote(self):
         return self.will_vote
@@ -41,47 +45,59 @@ class Green_Agent():
         # this may cause precision bugs!.. 
         return rand.randint(0, 1000) / 1000
 
+    def get_connections(self) -> list:
+        return self.connections
+
+    def add_connection(self, conn: int) -> None:
+        self.connections.append(conn)
 
 def create_green_agents(num_green: int, connect_prob: list) -> list:
     green_agents = list()
-    green_adjlist = [ [] for _ in range(num_green)]
 
+    #green_adjlist = [ [] for _ in range(num_green)]
 
-
+    # calculate bionomials
     binom_range = np.arange(0, connect_prob[0]+1)
     binomial_pmf = binom.pmf(binom_range, connect_prob[0], connect_prob[1])
     
+    prob = (connect_prob[0] * connect_prob[1]) / 100
+
+    # construct num_green of Green Agents
     for i in range(num_green):
         new_agent = Green_Agent(connect_prob)
         green_agents.append(new_agent)
 
     
+    # Check for connections between green agents
     for i, agent in enumerate(green_agents):
 
         for j in range(i+1, num_green):
+
+            # When is j less than i we have already checked those connections
+            # Thats why we start at  i+1
             
-            g_index = agent.get_prob_index()
-            g_value = agent.get_prob_value()
-
+            agent_1_prob = agent.get_prob_value()
+            agent_2_prob = green_agents[j].get_prob_value()
+            
             # check if agent has a connection
-            if g_value < binomial_pmf[g_index]:
+            if (agent_1_prob < prob) and (agent_2_prob < prob):
+                agent.add_connection(j)
+                green_agents[j].add_connection(i)
 
-                if not j in green_adjlist[i]:
-                    green_adjlist[i].append(j)
-                    green_adjlist[j].append(i)
-
-
-    return (green_agents, green_adjlist)
+    return green_agents
 
 
 
-connection_prob = [10, 0.4]
 
-num_green = 5
+connection_prob = [100, 0.4]
 
-g_agents, g_adjlist = create_green_agents(num_green, connection_prob)
+num_green = 20
 
-for i in g_adjlist:
-    for j in i:
+g_agents = create_green_agents(num_green, connection_prob)
+
+for i, agent in enumerate(g_agents):
+    adj_list = agent.get_connections()
+    print(f"Agent #{i}: ", end="")
+    for j in adj_list:
         print(f'{j} ', end="")
     print()
