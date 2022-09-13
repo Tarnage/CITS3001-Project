7bc08bc834b6c020cbbbedfb8ef0ae62a62a63e3
Tarnage <70882644+Tarnage@users.noreply.github.com>
2022-09-13 14:47:05 +0800

    changed to ref index instead of object

---
+++ src/Metrics.py
@@ -0,0 +1,46 @@
+from matplotlib import pyplot as plt
+import networkx as nx
+
+class Metrics:
+    def __init__(self) -> None:
+        return
+
+    def print_green_adjlist(self, network_list):
+        for i, agent in enumerate(network_list):
+            adj_list = agent.get_connections()
+            print(f"Agent #{i}: ", end="")
+            for j in adj_list:
+                print(f'{j} ', end="")
+            print()
+
+    def display_network(self, nx_graph: nx, display="graph"):
+            # Print the number of connections a green agent has
+            #print(nx.degree(nx_graph))
+
+            if display == "graph":
+                # print the current green network
+                pos = nx.circular_layout(nx_graph)
+
+                plt.clf()
+                nx.draw(nx_graph, pos, with_labels=1)
+                plt.show(block=False)
+                plt.pause(0.01)
+                
+
+            if display == "distribution":
+                # This plot should resemble a bionomoial distribution
+
+                all_degrees = list(dict((nx.degree(nx_graph))).values())
+                unique_degrees = list(set(all_degrees))
+                unique_degrees.sort()
+                nodes_with_degrees = list()
+
+                for i in unique_degrees:
+                    nodes_with_degrees.append(all_degrees.count(i))
+                
+                plt.plot(unique_degrees, nodes_with_degrees)
+                plt.xlabel("Connections")
+                plt.ylabel("No. of Green Agents")
+                plt.title("Green Agent Connection Distribution")
+                plt.show()
+            
--- src/main.py
+++ src/main.py
@@ -1,74 +1,78 @@
-from turtle import color
 import Agents
 import numpy
 import random as rand
 import networkx as nx
 import matplotlib.pyplot as plt
-1
+import Metrics
+
+
 class InfoSimulator:
     def __init__(self, uncert_ints: list, n: int, p: int, grey_proportion: int) -> None:
+        self.social_network = list()
         self.red_agent = Agents.Red_Agent()
         self.blue_agent = Agents.Blue_Agent()
         self.grey_agent = Agents.Grey_Agent(grey_proportion)
-        self.green_list = list()
+        self.metrics = Metrics.Metrics()
         self.n = n
         self.p = p
-                
+
+        self.social_network.append(self.red_agent)
+        self.social_network.append(self.blue_agent)
+        self.social_network.append(self.grey_agent)
+
         # Create a graph for modelling
-        self.g = nx.Graph()
-        
+        self.model = nx.Graph()
         self.create_green_agents(self.n, self.p)
-        
-        
-
-
-    
 
     def create_green_agents(self, n: int, p: list) -> None:
-        
-        self.green_list = list()
 
         # construct num_green of Green Agents
         for i in range(n):
             new_agent = Agents.Green_Agent()
-            self.green_list.append(new_agent)
+            self.social_network.append(new_agent)
             #adding red agent to connect to all
             self.red_agent.connections.append(new_agent)
             #connecting the red to everything
 
 
         # Check for connections between green agents
-        for i, agent in enumerate(self.green_list):
+        for i, agent in enumerate(self.social_network):
 
             for j in range(i+1, n):
 
                 # When is j less than i we have already checked those connections
                 # Thats why we start at  i+1
-                
-                agent_1_prob = agent.get_prob_value()
-                agent_2_prob = self.green_list[j].get_prob_value()
-                
-                # check if agent has a connection
-                # we can just compare one agent instead of two.
-                # We should simulate what happens
-                if (agent_1_prob < p) and (agent_2_prob < p):
 
-                    # add to g for modelling
-                    self.g.add_edge(i, j)
+                    team_one = agent.get_team()
+                    team_two = self.social_network[j].get_team()
 
-                    # add to agents adjlist
-                    agent.add_connection(j)
-                    self.green_list[j].add_connection(i)
+                    if not (team_one == "green") and \
+                       (team_two == "green"):
+
+                        agent_1_prob = agent.get_prob_value()
+                        agent_2_prob = self.social_network[j].get_prob_value()
+                        
+                        # check if agent has a connection
+                        # we can just compare one agent instead of two.
+                        # We should simulate what happens
+                        if (agent_1_prob < p) and (agent_2_prob < p):
+
+                            # add to g for modelling
+                            self.model.add_edge(i, j)
+
+                            # add to agents adjlist
+                            agent.add_connection(j)
+                            self.social_network[j].add_connection(i)
 
 
     def run(self):
         while self.blue_agent.get_energy() > 0 :
             self.Red_Turn()
-            self.print_distrubution_graph(display="graph")
+            self.metrics.display_network(self.model, display="graph")
             self.Blue_Turn()
-            self.print_distrubution_graph(display="graph")
+            self.metrics.display_network(self.model, display="graph")
             self.Green_Turn()
-            self.print_distrubution_graph(display="graph")
+            self.metrics.display_network(self.model, display="graph")
 
 
 
@@ -86,9 +90,6 @@ class InfoSimulator:
 
         # Change remaining green opinion
         self.Change_Opinion(opinionGain[option], False)
-
-        self.create_green_agents(self.n, self.p)
-
         return
 
 
@@ -113,21 +114,21 @@ class InfoSimulator:
     def Green_Turn(self):
         #set their current side
         opinionChange = 5 
-        for agent in self.green_list:
+        for agent in self.social_network:
             agent.current_side()
             #Mingle with eachother and effect opinions
             for connection in agent.connections:
                 if agent.get_side():
-                    self.green_list[connection].add_vote(opinionChange)
+                    self.social_network[connection].add_vote(opinionChange)
                 else:
-                   self.green_list[connection].add_not_vote(opinionChange)
+                   self.social_network[connection].add_not_vote(opinionChange)
 
         # Adding a grey will give it an opinion 
         return
 
 
     def Lose_Followers( self , percentage: int):
-        numA = len(self.red_agent.connections)
+        numA = len(self.red_agent.connections) - 1
         for i in range(int(numA*percentage/100)):
             #just removing random agents for now
             node = rand.randrange(numA)
@@ -135,9 +136,13 @@ class InfoSimulator:
         return
 
     def Change_Opinion(self, amount: int, voting: bool):
+
         if voting: # affecting everyone for now 
-            for agent in self.green_list:
-                agent.add_vote(amount)
+            for agent in self.social_network:
+                if  not isinstance(agent, Agents.Blue_Agent) and \
+                    not isinstance(agent, Agents.Red_Agent) and \
+                    not isinstance(agent, Agents.Grey_Agent):
+                        agent.add_vote(amount)
         else:  
             for agent in self.red_agent.connections:
                 agent.add_not_vote(amount)
@@ -152,51 +157,6 @@ class InfoSimulator:
             for conn in conn_list:
                 self.g.add_edge(i, conn)
 
-
-
-    def print_green_adjlist(self):
-        for i, agent in enumerate(self.green_list):
-            adj_list = agent.get_connections()
-            print(f"Agent #{i}: ", end="")
-            for j in adj_list:
-                print(f'{j} ', end="")
-            print()
-
-
-    def print_distrubution_graph(self, display="graph"):
-            self.g = nx.Graph()
-            self.g.add_nodes_from(range(0, self.n))
-            self.add_connections()
-            # Print the number of connections a green agent has
-            print(nx.degree(self.g))
-
-            if display == "graph":
-                # print the current green network
-                pos = nx.circular_layout(self.g)
-
-                plt.clf()
-                nx.draw(self.g, pos, with_labels=1)
-                plt.show(block=False)
-                plt.pause(0.01)
-                
-
-            if display == "distribution":
-                # This plot should resemble a bionomoial distribution
-
-                all_degrees = list(dict((nx.degree(self.g))).values())
-                unique_degrees = list(set(all_degrees))
-                unique_degrees.sort()
-                nodes_with_degrees = list()
-
-                for i in unique_degrees:
-                    nodes_with_degrees.append(all_degrees.count(i))
-                
-                plt.plot(unique_degrees, nodes_with_degrees)
-                plt.xlabel("Connections")
-                plt.ylabel("No. of Green Agents")
-                plt.title("Green Agent Connection Distribution")
-                plt.show()
-            
 
 if __name__ == "__main__":
 
