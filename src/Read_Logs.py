c5e3e6f20e328ba1c775f24203add8a915518392
Tarnage <70882644+Tarnage@users.noreply.github.com>
2022-10-10 11:35:32 +0800

    ..

--- src/Read_Logs.py
+++ src/Read_Logs.py
@@ -1,34 +1,86 @@
 import os
 
+class Logs:
+    def __init__(self, team) -> None:
+        self.turns = 0
+        self.team = team
+        self.uncert = [0]
+        self.followers = list()
+        self.followers_gained = [0]
+        self.followers_lost = [0]
+        self.total_green = 0
+
+    def init_followers(self, followers: int):
+        self.followers.append(followers)
+
+    def init_green(self, tot_green: int):
+        self.total_green = tot_green
+
+    def get_turns(self):
+        return self.turns
+
+    def get_team(self):
+        return self.team
+
+    def set_team(self, team: str):
+        self.team = team
+
+    def get_uncert(self, index: int):
+        if index < len(self.uncert) and index >= 0:
+            return self.uncert[index]
+        else:
+            print("Index Out of Bounds")
+
+    def get_followers(self, index: int):
+        # each index of the curr_follwers list represents the turn. i.e curr_follower[2] will return the followers at turn 2
+        if index < len(self.uncert) and index >= 0:
+            return self.uncert[index]
+        else:
+            print("Index Out of Bounds")
+
+    def set_followers(self, followers: int):
+        # each index of the curr_follwers list represents the turn. i.e curr_follower[2] will return the followers at turn 2
+        self.followers.append(followers)
+
+    def get_followers_lost(self, index: int):
+        # each index of the curr_follwers list represents the turn. i.e curr_follower[2] will return the followers at turn 2
+        if index < len(self.uncert) and index >= 0:
+            return self.followers_lost[index]
+        else:
+            print("Index Out of Bounds")
+
+    def set_followers(self, followers: int):
+        # each index of the curr_follwers list represents the turn. i.e curr_follower[2] will return the followers at turn 2
+        self.followers.append(followers)
+
+    def set_total_green(self, input: int):
+        self.total_green = input
 
 
 class Read_Logs:
     def __init__(self) -> None:
-        self.results =  {
-                            "red": {
-                                "turn": {}
-                            },
-                            "blue": {
-                                "turn": {}
-                            },
-                            "green": {
-                                "turn": {}
-                            },
-                            "grey": {
-                                "team":{
-                                    "blue": {},
-                                    "red": {}
-                                }
-                            }
-                        }
+        self.results = dict()
 
 
     def read_logs(self, log: str):
 
-        with open(log, 'r') as input:
-            pass
+        red = Logs("red")
+        blue = Logs("blue")
+        green = Logs("green")
+        grey = Logs("grey")
 
+        with open(log, 'r') as input:
+            
+            files = input.readlines()
+
+            index = 2
+            length = len(files)
+
+            while index < length:
+
+                pass
     
+
     def read_all_logs(self, path: str):
         
         # Change the directory
@@ -43,3 +95,9 @@ class Read_Logs:
                 # call read text file function
                 self.read_logs(file_path)
         
+
+if __name__ == "__main__":
+    path = ".\logs\Game_21.log"
+    log_reader = Read_Logs()
+
+    log_reader.read_logs(path)
