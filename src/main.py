d0f98c125378cd5d4415256c3b90771ed5cce321
Tarnage <70882644+Tarnage@users.noreply.github.com>
2022-10-16 15:30:50 +0800

    added command line args

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
 from InfoSimulator import *
 import sys
 
-# Redirecting the print function to the void
-sys.stdout = open(os.devnull, 'w')
+
+
+
+def get_interval():
+    broad_interval = [-0.5, 0.5]
+    tight_interval = [-0.9, 0.1]
+
+    while True:
+        option = input("Choose Uncertainty Intervals")
+        try:
+            if option.isdigit():
+                if int(option) > 6:
+                    raise ValueError
+
+            elif option == "quit":
+                raise Exception
+            break
+        except ValueError:
+            print("This is not a number. Please enter a valid number or type 'quit' to close the game")
+        except Exception:
+            print("Gracefully quiting game")
+            exit(0)
+
+    return int(option)
+
 
 if __name__ == "__main__":
 
@@ -17,8 +40,20 @@ if __name__ == "__main__":
     grey_proportion_high = 0.8 # 80% chance grey is working for Red team
     grey_proportion_low = 0.1 # 10% chance grey is working for Red team
 
-    sim = InfoSimulator(tight_interval, connect_prob_1[0], connect_prob_1[1], grey_proportion_low)
-    sim.run()
+    simulate = False
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "simulate":
+            simulate = True
+
+    if simulate:
+        # Redirecting the print function to the void
+        sys.stdout = open(os.devnull, 'w')
+        sim = InfoSimulator(broad_interval, connect_prob_1[0], connect_prob_1[1], grey_proportion_low, simulate=simulate)
+        sim.run()
+    else:
+        sim = InfoSimulator(broad_interval, connect_prob_1[0], connect_prob_1[1], grey_proportion_low)
+        sim.run()
     #sim.print_distrubution_graph(display="distribution")
     #sim.print_distrubution_graph(display="graph")
 
